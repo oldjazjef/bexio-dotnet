4a5d5b2cb78b7509a60761aa1be6b41a2658fb3a
Emanuel Mistretta <emanuel.mistretta@etrex.ch>
2021-08-03 10:11:08 +0200

    remapped the filter field constants
    Introduced backlogs for all filter fields to prevents multiple updates on value changes

--- bexio-api/bexio-lib/Data/BexioConstants.cs
+++ bexio-api/bexio-lib/Data/BexioConstants.cs
@@ -71,37 +71,68 @@ namespace bexio_lib.Data
         public const string ORDER_BY = "order_by";
     }
 
-
-    public static class BexioCountryFilterFields
-    {
-        public const string name = "name";
-        public const string name_short = "name_short";
-    }
-
-    public static class BexioCurrencyFilterFields
-    {
-        public const string name = "name";
-    }
-
-    public static class BexioInvoiceFilterFields
+    public static class BexioFilterFieldsBacklog
     {
         public const string id = "id";
         public const string document_nr = "document_nr";
         public const string api_reference = "api_reference";
         public const string kb_item_status_id = "kb_item_status_id";
+        public const string title = "title";
+        public const string contact_id = "contact_id";
+        public const string contact_sub_id = "contact_sub_id";
+        public const string user_id = "user_id";
+        public const string currency_id = "currency_id";
+        public const string total_gross = "total_gross";
+        public const string total_net = "total_net";
+        public const string total = "total";
+        public const string is_valid_from = "is_valid_from";
+        public const string is_valid_to = "is_valid_to";
+        public const string intern_code = "intern_code";
+        public const string intern_name = "intern_name";
+        public const string name = "name";
+        public const string name_short = "name_short";
+    }
+
+    public static class BexioCountryFilterFields
+    {
+        public const string name = BexioFilterFieldsBacklog.name;
+        public const string name_short = BexioFilterFieldsBacklog.name_short;
+    }
+
+    public static class BexioCurrencyFilterFields
+    {
+        public const string name = BexioFilterFieldsBacklog.name;
+    }
+
+    public static class BexioInvoiceFilterFields
+    {
+        public const string id = BexioFilterFieldsBacklog.id;
+        public const string document_nr = BexioFilterFieldsBacklog.document_nr;
+        public const string api_reference = BexioFilterFieldsBacklog.api_reference;
+        public const string kb_item_status_id = BexioFilterFieldsBacklog.kb_item_status_id;
+        public const string title = BexioFilterFieldsBacklog.title;
+        public const string contact_id = BexioFilterFieldsBacklog.contact_id;
+        public const string contact_sub_id = BexioFilterFieldsBacklog.contact_sub_id;
+        public const string user_id = BexioFilterFieldsBacklog.user_id;
+        public const string currency_id = BexioFilterFieldsBacklog.currency_id;
+        public const string total_gross = BexioFilterFieldsBacklog.total_gross;
+        public const string total_net = BexioFilterFieldsBacklog.total_net;
+        public const string total = BexioFilterFieldsBacklog.total;
+        public const string is_valid_from = BexioFilterFieldsBacklog.is_valid_to;
+        public const string is_valid_to = BexioFilterFieldsBacklog.is_valid_to;
     }
 
     public static class BexioItemFilterFields
     {
-        public const string id = "id";
-        public const string intern_code = "intern_code";
-        public const string intern_name = "intern_name";
+        public const string id = BexioFilterFieldsBacklog.id;
+        public const string intern_code = BexioFilterFieldsBacklog.intern_code;
+        public const string intern_name = BexioFilterFieldsBacklog.intern_name;
     }
 
     public static class BexioOrderFilterFields
     {
-        public const string kb_item_status_id = "kb_item_status_id";
-        public const string title = "title";
-        public const string document_nr = "document_nr";
+        public const string kb_item_status_id = BexioFilterFieldsBacklog.kb_item_status_id;
+        public const string title = BexioFilterFieldsBacklog.title;
+        public const string document_nr = BexioFilterFieldsBacklog.document_nr;
     }
 }
